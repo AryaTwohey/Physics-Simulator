a175b49e860b04588376faba7c21eb0093e03973
Arya Twohey <atwohey@bayviewglen.ca>
2023-03-10 00:00:43 -0500

    Nothing done here yet

---
+++ Physics Simulator Files/JS/calculations.js
@@ -0,0 +1,21 @@
+
+let forceApplied = document.querySelector('#fA')
+let forceFriction = document.querySelector('#fF')
+let mass = document.querySelector('#m')
+let incline = document.querySelector('#incline')
+let displacement = document.querySelector("#deltaX")
+function calculateInitalVelocity(){
+//couple of things to consider
+
+//adding prompts to say if the object is starting from rest, constantl velocty, which direction it is going
+//all of these should be check boxes
+
+
+
+}
+function calculateAcceleration(){
+
+}
+
+
+
