09377328099a1ebdda0542d54823f43618fcc792
Arya Twohey <atwohey@bayviewglen.ca>
2023-03-10 17:19:43 -0500

    Added A Slider

--- Physics Simulator Files/JS/calculations.js
+++ Physics Simulator Files/JS/calculations.js
@@ -1,14 +1,22 @@
+const forceApplied = document.querySelector("#faSlider").value
 
-let forceApplied = document.querySelector('#fA')
-let forceFriction = document.querySelector('#fF')
-let mass = document.querySelector('#m')
-let incline = document.querySelector('#incline')
-let displacement = document.querySelector("#deltaX")
-function calculateInitalVelocity(){
+// let forceApplied = document.querySelector('#')
+// let forceFriction =document.querySelector('#fF')
+// let mass = document.querySelector('#m')
+// let incline = document.querySelector('#incline')
+// let displacement = document.querySelector("#deltaX")
+
+
+
+function calculateFinalVelocity(){
 //couple of things to consider
 
 //adding prompts to say if the object is starting from rest, constantl velocty, which direction it is going
-//all of these should be check boxes
+//all of these should be check boxes//
+
+//W = 1/2vf^2 - 1/2 vi^2 + ugh
+
+
 
 
 
@@ -18,4 +26,7 @@ function calculateAcceleration(){
 }
 
 
-
+function printStuff(){
+    console.log(forceApplied);
+    
+}
